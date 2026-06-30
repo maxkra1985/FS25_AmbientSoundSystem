9d896ab6526fe6b202a5f5750d770fdb4f1b60c2
maxkra1985 <64097861+maxkra1985@users.noreply.github.com>
2026-06-30 07:50:53 +0300

    refactoring v2
    рефакторинг в2

--- scripts/events/AmbientSoundStopEvent.lua
+++ scripts/events/AmbientSoundStopEvent.lua
@@ -1,23 +1,11 @@
--- ЧАСТЬ 1
 ------------------------------------------------------------------------------
 -- AmbientSoundStopEvent.lua
---
--- Сетевое событие остановки глобального звука.
---
--- Сервер сообщает клиентам:
---   • какой звук остановить.
---
--- Клиент удаляет активный экземпляр.
+-- Сервер -> Клиенты
+-- Остановка и удаление глобального экземпляра звука.
 ------------------------------------------------------------------------------
-
 AmbientSoundStopEvent = {}
-
-
 local AmbientSoundStopEvent_mt = Class(AmbientSoundStopEvent, Event)
 
-------------------------------------------------------------------------------
--- Регистрация события
-------------------------------------------------------------------------------
 InitEventClass(AmbientSoundStopEvent, "AmbientSoundStopEvent")
 
 ------------------------------------------------------------------------------
@@ -31,77 +19,71 @@ end
 ------------------------------------------------------------------------------
 -- Конструктор
 ------------------------------------------------------------------------------
-
----@param soundId number
----@param instanceId number
-------------------------------------------------------------------------------
-function AmbientSoundStopEvent.new( soundId, instanceId)
+function AmbientSoundStopEvent.new(runtimeId)
 	local self = AmbientSoundStopEvent.emptyNew()
-	self.soundId = soundId
-	self.instanceId = instanceId
+	self.runtimeId = runtimeId
 	return self
 end
 
 ------------------------------------------------------------------------------
--- Запись данных
+-- Запись в поток
 ------------------------------------------------------------------------------
-
 function AmbientSoundStopEvent:writeStream(streamId, connection)
-	streamWriteInt32(streamId,self.soundId)
-	streamWriteInt32(streamId,self.instanceId)
+	streamWriteUInt16(streamId, self.runtimeId)
 end
 
 ------------------------------------------------------------------------------
--- Чтение данных
+-- Чтение из потока
 ------------------------------------------------------------------------------
-
 function AmbientSoundStopEvent:readStream(streamId, connection)
-	self.soundId = streamReadInt32(streamId)
-	self.instanceId = streamReadInt32(streamId)
+	self.runtimeId = streamReadUInt16(streamId)
+	self:run(connection)
 end
 
--- ЧАСТЬ 2
 ------------------------------------------------------------------------------
--- Выполнение события
+-- Выполнение на клиенте
 ------------------------------------------------------------------------------
-
-function AmbientSoundStopEvent:run()
-	if g_ambientSoundSystem == nil then
-		AmbientSoundUtil.warning("AmbientSoundSystem отсутствует при остановке звука")
+function AmbientSoundStopEvent:run(connection)
+	if AmbientSoundUtil.isServer() then
 		return
 	end
-
-	for index = #g_ambientSoundSystem.activeSounds, 1, -1 do
-		local sound = g_ambientSoundSystem.activeSounds[index]
-		if sound.config.id == self.soundId then
-			sound:stop()
-			sound:delete()
-			table.remove(g_ambientSoundSystem.activeSounds,index)
-			AmbientSoundUtil.debug("Остановлен глобальный звук ID=%d",self.soundId)
-			break
-		end
-	end
-end
-
-------------------------------------------------------------------------------
--- Отправка события сервером
-------------------------------------------------------------------------------
-
---- Отправляет команду остановки всем клиентам.
----@param sound table
----@param instanceId number
-------------------------------------------------------------------------------
-function AmbientSoundStopEvent.sendEvent(sound, instanceId)
-	if g_server == nil then
+	local system = g_ambientSoundSystem
+	if system == nil then
 		return
 	end
-	local event = AmbientSoundStopEvent.new(sound.id, instanceId or 0)
-	g_server:broadcastEvent(event,false)
-	AmbientSoundUtil.debug("Отправлена остановка глобального звука ID=%d", sound.id)
+	local runtime = system:getRuntimeSound(self.runtimeId)
+	if runtime == nil then
+		AmbientSoundUtil.warning("StopEvent: Runtime #%d не найден.", self.runtimeId)
+		return
+	end
+	runtime:delete()
+	system.activeSounds[self.runtimeId] = nil
+	AmbientSoundUtil.debug("Удалён Runtime #%d", self.runtimeId)
 end
 
 ------------------------------------------------------------------------------
--- Завершение загрузки
+-- Отправка события
 ------------------------------------------------------------------------------
+function AmbientSoundStopEvent.sendEvent(runtimeId)
+	if not AmbientSoundUtil.isServer() then
+		return
+	end
+	g_server:broadcastEvent(AmbientSoundStopEvent.new(runtimeId), nil, nil)
+end
 
-AmbientSoundUtil.info("Модуль AmbientSoundStopEvent загружен")
+------------------------------------------------------------------------------
+-- Проверка данных
+------------------------------------------------------------------------------
+function AmbientSoundStopEvent:validate()
+	if self.runtimeId == nil then
+		return false
+	end
+	return true
+end
+
+------------------------------------------------------------------------------
+-- Отладочная информация
+------------------------------------------------------------------------------
+function AmbientSoundStopEvent:printDebug()
+	AmbientSoundUtil.debug("StopEvent Runtime=%d", self.runtimeId)
+end
